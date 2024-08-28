ed916c5a0e6925b6e5c04f30ed0f2f4f804b9f56
Kevin Maurizi <kevin.maurizi@bzz.ch>
2024-08-28 11:17:24 +0200

    init

--- main.py
+++ main.py
@@ -1,12 +1,35 @@
-def calculate():
+def add_grade(student, grade):
     """
-    calculates the total
-    :return: None
+    Returns a new Student instance with the added grade.
     """
-    price = 14.75
-    quantity = 3  # TODO change quantity to 5
-    print(f'Total: {price * quantity}')
+    # todo: implement this function
+    pass
+
+def calculate_average(student):
+    """
+    Returns the average of the student's grades.
+    """
+    # todo: implement this function
+    pass
+
+
+def graduate_student(student):
+    """
+    Graduates the student if the average grade is 70 or above.
+    """
+    # todo: implement this function
+    pass
 
 
 if __name__ == '__main__':
-    calculate()
+    student = Student(name='John Doe')
+    student = add_grade(student, 85)
+    student = add_grade(student, 75)
+    student = add_grade(student, 60)
+
+    print(f'Noten: {student.grades}')
+    average = calculate_average(student)
+    print(f'Durchschnitt: {average}')
+
+    student = graduate_student(student)
+    print(f'Absolviert: {student.graduated}')
